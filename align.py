cd7d2428cf8c805b242bf437a2e891148883baea
James Lucas <james.lucas@berkeley.edu>
2017-02-19 10:15:13 +0800

    Slowly converting the scoring matrix from pandas dataframe to numpy matrix...

--- align.py
+++ align.py
@@ -43,8 +43,8 @@ class align(object):
 
         """
 
-        self.scoring_matrix = pd.DataFrame(columns= np.arange(len(self.seq_A) + 1), index= np.arange(len(self.seq_B) + 1))
-
+        # self.scoring_matrix = pd.DataFrame(columns= np.arange(len(self.seq_A) + 1), index= np.arange(len(self.seq_B) + 1))
+        self.scoring_matrix = np.full((len(self.seq_B) + 1, len(self.seq_A) + 1), -1) # [ rows x columns ]
 
     def fill_in_matrix(self):
         """
@@ -54,12 +54,12 @@ class align(object):
 
         # Initialize 0-indexed row and column
         for column in np.arange(len(self.seq_A) + 1):
-            self.scoring_matrix.ix[0, column] = 0
+            self.scoring_matrix[0, column] = 0
         for row in np.arange(len(self.seq_B) + 1):
-            self.scoring_matrix.ix[row, 0] = 0
+            self.scoring_matrix[row, 0] = 0
 
         # Iterate over rows and fill in the matrix based on substitution matrix
-        for row_index, row in self.scoring_matrix.iterrows():
+        for row_index, row in enumerate(self.scoring_matrix):
             for column_index in np.arange(len(row)):
                 if row_index > 0 and column_index > 0:
 
@@ -73,9 +73,9 @@ class align(object):
                         gap_penalty = self.gap_extension_penalty
 
                     # Calculate possible scores for current cell in scoring matrix
-                    score_up = self.scoring_matrix.ix[row_index - 1, column_index] + gap_penalty
-                    score_left = self.scoring_matrix.ix[row_index, column_index] + gap_penalty
-                    score_diag = self.scoring_matrix.ix[row_index - 1, column_index - 1] + similarity_score
+                    score_up = self.scoring_matrix[row_index - 1, column_index] + gap_penalty
+                    score_left = self.scoring_matrix[row_index, column_index] + gap_penalty
+                    score_diag = self.scoring_matrix[row_index - 1, column_index - 1] + similarity_score
 
                     # Take highest final score and assign to current in scoring matrix
                     final_score = max(score_up, score_left, score_diag)
@@ -86,22 +86,25 @@ class align(object):
                     else:
                         self.extending_gap = True
 
-                    self.scoring_matrix.ix[row_index, column_index] = final_score if final_score > 0 else 0
+                    self.scoring_matrix[row_index, column_index] = final_score if final_score > 0 else 0
 
-        self.scoring_matrix.to_csv('SCORE_current.csv')
+        assert len(np.argwhere(self.scoring_matrix == -1)) == 0, "The scoring matrix was not completely filled in!"
+        np.savetxt("SCORE_current.csv", self.scoring_matrix, delimiter=",")
 
     def traceback(self):
         # Find maximum score in matrix
 
-        row_max_indicies = self.scoring_matrix.max(axis=0)
-        seq_B_high_score = row_max_indicies.idxmax(axis=0)
-        seq_A_high_score = self.scoring_matrix.idxmax(axis=0)
+        row_max_indicies = self.scoring_matrix.argmax(axis=0)
+        seq_B_high_score = row_max_indicies.argmax(axis=0)
+        seq_A_high_score = self.scoring_matrix.argmax(axis=0)
 
         column_index_high = seq_B_high_score
         row_index_high = seq_A_high_score[column_index_high]
 
         print("Starting Column index: {}".format(column_index_high))
         print("Starting Row index: {}".format(row_index_high))
+
+        sys.exit()
 
         # Initialize variables needed for traceback
         current_score = self.scoring_matrix.ix[row_index_high, column_index_high]
@@ -189,3 +192,5 @@ if __name__ == '__main__':
         seq_align.initialize_scoring_matrix()
         seq_align.fill_in_matrix()
         seq_align.traceback()
+
+        break
