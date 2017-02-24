57624abe54f147ccda8dd79b16ae35641d687089
James Lucas <james.lucas@berkeley.edu>
2017-02-24 10:46:21 +0800

    y

--- test/test_align.py
+++ test/test_align.py
@@ -21,31 +21,31 @@ def test_align():
         seq_align.working_pairs = [test_pair]
         A, bar, B = run_alignments(seq_align)
 
-        if test_pair == "test/test_1.fa  test/test_2.fa":
+        if test_pair.strip() == "test/test_1.fa  test/test_2.fa":
             assert A == "RRRRRAAAAA", (A, type(A))
             assert bar == "||||||||||", (bar, type(bar))
             assert B == "RRRRRAAAAA", (B, type(B))
 
-        elif test_pair == "test/test_3.fa  test/test_4.fa":
+        elif test_pair.strip() == "test/test_3.fa  test/test_4.fa":
             assert A == None, (A, type(A))
             assert bar == None, (bar, type(bar))
             assert B == None, (B, type(B))
 
-        elif test_pair == "test/test_1.fa  test/test_3.fa":
+        elif test_pair.strip() == "test/test_1.fa  test/test_3.fa":
             assert A == "WWWWWWWWWWWWWWW", (A, type(A))
             assert bar == "|||||||||||||||", (bar, type(A))
             assert B == "WWWWWWWWWWWWWWW", (B, type(A))
 
-        elif test_pair == "test/test_2.fa  test/test_4.fa":
+        elif test_pair.strip() == "test/test_2.fa  test/test_4.fa":
             assert A == "AAAAAAAAAAAAAAA", (A, type(A))
             assert bar == "|||||||||||||||", (bar, type(A))
             assert B == "AAAAAAAAAAAAAAA", (B, type(A))
 
-        elif test_pair == "test/test_5.fa  test/test_6.fa":
+        elif test_pair.strip() == "test/test_5.fa  test/test_6.fa":
             assert A == "ASDFASDFASDF--DFASD--SDFASDFASDFASDF", (A, type(A))
             assert bar == "||:|||:|||:|  :|||:  |:|||:|||:|||:|", (bar, type(A))
             assert B == "ASHFASHFASHFASHFASHFASHFASHFASHFASHF", (B, type(A))
-            
+
         else:
-            assert False==True, "How did you get here"
+            assert False==True, (test_pair, type(test_pair))
 
